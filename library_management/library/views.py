788d8b3e861dbbfa4ad5df3139ebc181a99c5dc6
Ashish Bhardwaj <erashish41@gmail.com>
2025-07-19 23:27:42 +0530

    book list added

--- library_management/library/views.py
+++ library_management/library/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from library.models import Book, LibraryBranch
+from django.views.generic import ListView
+
 
 # Create your views here.
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
+    paginate_by = 10
+    
+class LibraryBranchView(ListView):
+    model = LibraryBranch
+    template_name = 'branches/library_branches.html'
--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'library_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

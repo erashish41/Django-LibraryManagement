be3944b423a27df34f299192463451c4a932fec6
Ashish Bhardwaj <erashish41@gmail.com>
2025-07-29 20:46:37 +0530

    overriding generic view method

--- library_management/library/api_modules/urls.py
+++ library_management/library/api_modules/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path
-from library.api_modules.views import ListCreateMember
+from library.api_modules.views import (
+    ListCreateIssuedBook, RetrieveUpdateDestroyIssuedBook,
+    ListCreateMember, RetrieveUpdateDestroyMember,
+    ListCreateBook, RetrieveUpdateDestroyBook,
+    ListCreateLibraryBranch, RetrieveUpdateDestroyLibraryBranch
+    )
 
 urlpatterns = [
-    path('members/',ListCreateMember.as_view(),name='member_list')
+    
+    path('issuedbooks/',ListCreateIssuedBook.as_view(),name='issuedbook_list'),
+    path('issuedbooks/<uuid:pk>/',RetrieveUpdateDestroyIssuedBook.as_view(),name='issuedbook_detail'),
+    
+    path('issuedbooks/<uuid:issuedbook_pk>/books/',ListCreateBook.as_view(),name='book_list'),
+    path('issuedbooks/<uuid:issuedbook_pk>/books/<uuid:pk>/',RetrieveUpdateDestroyBook.as_view(),name='book_detail'),
+    
+    # path('librarybranches/',ListCreateLibraryBranch.as_view(),name='branch_list'),
+    # path('librarybranches/<uuid:pk>/',RetrieveUpdateDestroyLibraryBranch.as_view(),name='librarybranche_detail'),
+    
+    
+    
+    # path('members/',ListCreateMember.as_view(),name='member_list'),
+    # path('members/<uuid:pk>/',RetrieveUpdateDestroyMember.as_view(),name='member_detail'),
 ]
--- library_management/library/api_modules/views.py
+++ library_management/library/api_modules/views.py
@@ -1,23 +1,51 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
 from library.models import Book, LibraryBranch, Member, IssuedBook, Author, Publisher
 from library.api_modules.serializers import IssuedBookSerializer, MemberSerializer, BookSerializer, AuthorSerializer, CategorySerializer, LibraryBranchSerializer, CitySerializer
 
-class ListCreateMember(APIView):
+
+class ListCreateIssuedBook(generics.ListCreateAPIView):
+    queryset = IssuedBook.objects.all()
+    serializer_class = IssuedBookSerializer
     
-    def get(self,request,format=None):
-        print(">>>>>.get method called")
-        member = Member.objects.all()
-        serializer = MemberSerializer(member,many=True)
-        return Response(serializer.data)
+class RetrieveUpdateDestroyIssuedBook(generics.RetrieveUpdateDestroyAPIView):
+    queryset = IssuedBook.objects.all()
+    serializer_class = IssuedBookSerializer
+
+class ListCreateMember(generics.ListCreateAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
     
-    def post(self,request,format=None):
-        print(">>>>>.post method called")
-        serializer = MemberSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        print(">>>>status>>>>", status)
-        print(">>>>status>>>>", serializer.data)
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
+class RetrieveUpdateDestroyMember(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+    
+
+class ListCreateBook(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(issued_records__id = self.kwargs.get('issuedbook_pk'))
+    
+    def perform_create(self, serializer):
+        branch = get_object_or_404(LibraryBranch, pk= self.kwargs.get('issuedbook_pk'))
+        serializer.save(library_branch=branch)
+    
+class RetrieveUpdateDestroyBook(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def get_object(self):
+        return get_object_or_404(self.get_queryset(),
+                                issued_records__id = self.kwargs.get('issuedbook_pk'),
+                                pk = self.kwargs.get('pk'))
+
+class ListCreateLibraryBranch(generics.ListCreateAPIView):
+    queryset = LibraryBranch.objects.all()
+    serializer_class = LibraryBranchSerializer
+    
+class RetrieveUpdateDestroyLibraryBranch(generics.RetrieveUpdateDestroyAPIView):
+    queryset = LibraryBranch.objects.all()
+    serializer_class = LibraryBranchSerializer
